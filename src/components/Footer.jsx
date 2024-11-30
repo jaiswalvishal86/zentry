b61445ded8417994c6ca19e25266cf0bf8b5f0d1
Vishal Jaiswal <vishal@Vishals-MacBook-Air.local>
2024-11-30 22:05:49 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import { FaDiscord, FaTwitch, FaTwitter } from "react-icons/fa";
+
+const links = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://twitch.com", icon: <FaTwitch /> },
+];
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-voilet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left">
+          &copy; Nove 2024. All rights reserved
+        </p>
+        <div className="flex gap-4 justify-center md:justify-start">
+          {links.map((link) => (
+            <a
+              key={link.href}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <a
+          href="#"
+          className="text-center text-sm hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
